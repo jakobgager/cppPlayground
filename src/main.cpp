02cd59a2c914bab7103d92f5d9a6dfeae61bd86b
Jakob Gager <jakobgager@hotmail.com>
2020-05-23 22:34:53 +0200

    VSCode Tutorial added (Hello world with STL)

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<string> msg {"Hello", "World"};
+    for (const string &word : msg) {
+        cout << word << " ";
+    }
+    cout << endl;
+    return 0;
+}
